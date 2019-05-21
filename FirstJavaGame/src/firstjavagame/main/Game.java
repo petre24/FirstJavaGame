f3feee2ed998529c5a3f0f0bd040bf4ac6072fd5
Petre Quintua <pm_quin_24@yahoo.com>
2019-05-21 02:27:42 -0700

    Fixed uneven indentation, again
    just to be at ease with myself with the indentation

--- FirstJavaGame/src/firstjavagame/main/Game.java
+++ FirstJavaGame/src/firstjavagame/main/Game.java
@@ -2,7 +2,7 @@ package firstjavagame.main;
 
 public class Game extends Canvas implements Runnable
 {
-    private static final long serialVersionUID = -8173324309904070247L;
+	private static final long serialVersionUID = -8173324309904070247L;
 
 	public static final int WIDTH = 640;
 	public static final int HEIGHT = WIDTH / 12 * 9;
