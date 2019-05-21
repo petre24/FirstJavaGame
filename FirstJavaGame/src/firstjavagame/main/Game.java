758eb77d1ffa30230e777be0b34f6ac1bf34b868
Petre Quintua <pm_quin_24@yahoo.com>
2019-05-21 02:25:57 -0700

    Allowed a window to be displayed to the user
    serial ID added, window aspect ratio created, start and stop methods created, communication between Window.java and Game.java is initiated

--- FirstJavaGame/src/firstjavagame/main/Game.java
+++ FirstJavaGame/src/firstjavagame/main/Game.java
@@ -1,9 +1,25 @@
 package firstjavagame.main;
 
-public class Game 
+public class Game extends Canvas implements Runnable
 {
-	public static void main(String[] args) {
+    private static final long serialVersionUID = -8173324309904070247L;
+
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = WIDTH / 12 * 9;
+
+	public Game() {
+		 new Window(WIDTH, HEIGHT, "First Game (Version 1.0)", this);
+	}
+	
+	public synchronized void start() {
 		
 	}
 	
+	public void run() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new Game();
+	}
 }
