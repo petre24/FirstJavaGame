e8c4596992449ae721f0f6dc1c8b4d2378e71d57
Petre Quintua <pm_quin_24@yahoo.com>
2019-05-21 02:33:26 -0700

    Added a thread for single-thread execution
    filled in stop and start methods, added a Thread, created a boolean value to know whether or not the game is currently running

--- FirstJavaGame/src/firstjavagame/main/Game.java
+++ FirstJavaGame/src/firstjavagame/main/Game.java
@@ -1,5 +1,7 @@
 package firstjavagame.main;
 
+import java.awt.Canvas;
+
 public class Game extends Canvas implements Runnable
 {
 	private static final long serialVersionUID = -8173324309904070247L;
@@ -7,16 +9,26 @@ public class Game extends Canvas implements Runnable
 	public static final int WIDTH = 640;
 	public static final int HEIGHT = WIDTH / 12 * 9;
 
+	private Thread thread;
+	private boolean running = false;
+
 	public Game() {
 		 new Window(WIDTH, HEIGHT, "First Game (Version 1.0)", this);
 	}
 
 	public synchronized void start() {
-
+		thread = new Thread(this);
+		thread.start();
+		running = true;
 	}
 
 	public void run() {
-
+		try {
+			thread.join();  // kills the thread
+			running = false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
