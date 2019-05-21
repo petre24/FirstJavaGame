d75e35da92e4f19ceacc7a05495a2a76f21ae84c
Petre Quintua <pm_quin_24@yahoo.com>
2019-05-21 02:06:42 -0700

    Added files via upload
    moved from Eclipse

---
+++ FirstJavaGame/src/firstjavagame/main/Game.java
@@ -0,0 +1,9 @@
+package firstjavagame.main;
+
+public class Game 
+{
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ FirstJavaGame/src/firstjavagame/main/Window.java
@@ -0,0 +1,5 @@
+package firstjavagame.main;
+
+public class Window {
+
+}
