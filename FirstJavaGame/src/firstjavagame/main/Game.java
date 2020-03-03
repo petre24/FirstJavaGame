d2cd655c47db3328b05b3dad995ae27e41183f0b
Petre <pm_quin_24@yahoo.com>
2020-03-02 19:03:03 -0800

    fiddled with the color of the window

--- FirstJavaGame/src/firstjavagame/main/Game.java
+++ FirstJavaGame/src/firstjavagame/main/Game.java
@@ -1,6 +1,9 @@
 package firstjavagame.main;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable
 {
